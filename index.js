646327fd2c7247fa1722a72e4f4df279c9123679
kipmaiyo <danielkipmaiyo44@gmail.com>
2023-06-22 15:30:04 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+
+const mainElement = document.querySelector("main")
+mainElement.remove()
+
+const newHeader =document.createElement("h1")
+newHeader.id = "victory"
+
+newHeader.innerHTML = "YOUR-NAME is the champion"
